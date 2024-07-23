bdc3aa7a0bd95262fdf3898e2b153e620242a354
tomo007 <tomislavblaz@gmail.com>
2024-07-23 18:56:50 +0200

    adding different examples for flask

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from flask import Flask
+from flask.json import jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__)  # Flask("main.py")
+
+
+dict_primjer = {"Ime": "Marko", "Prezime": "Markic", "Email": "marko@mail.com"}
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello world</p>"
+def home():
+    return "<h1><a href='/about'>Home page</a></h1>"
 
 
 @app.route("/about")
 def about_page():
     return "<p>About page</p>"
+
+
+@app.route("/user/<username>")
+def user(username):
+    return f"<h3>User page for {username}</h3>"
+
+
+@app.route("/json")
+def json():
+    return jsonify(dict_primjer)
+
+
+@app.route("/json/<key>")
+def json_value(key):
+    return dict_primjer.get(key, "Unknown key")
